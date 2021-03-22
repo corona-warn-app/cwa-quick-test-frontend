8154859ecaa7651f82a97808b4481f50b383650f
ggrund <gordon.grund@t-systems.com>
2021-03-22 08:53:14 +0100

    add header footer

--- src/components/landing-page.component.tsx
+++ src/components/landing-page.component.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
-import { Button, Card, Container } from 'react-bootstrap'
+import { Button, Card } from 'react-bootstrap'
 import { BrowserRouter, Link, Route, useHistory } from 'react-router-dom'
 import useNavigation from '../misc/navigation';
-import Routes from '../misc/routes';
 
 const LandingPage = (props: any) => {
     const navigation = useNavigation();
 
     return (
-        <Card className='h-100 p-3'>
+        <Card className='h-100 p-3 border-0'>
             <Card.Body className='d-flex flex-column'>
                 <Button className='m-2' variant='secondary' onClick={navigation.toLanding}>toLanding</Button>
                 <Button className='m-2' variant='secondary' onClick={navigation.toRecordPatient}>toRecordPatient</Button>
