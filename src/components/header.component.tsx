9e075770c9cf28fa0f10db7c0d62d2ac4ea9a9ad
ggrund-tsi <80682839+ggrund-tsi@users.noreply.github.com>
2021-03-22 09:56:49 +0100

    add header footer (#11)

--- src/components/header.component.tsx
+++ src/components/header.component.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
-import { Button, Container, Navbar } from 'react-bootstrap'
+import { Button, Container, Navbar, Row, Image } from 'react-bootstrap'
 import { BrowserRouter, Link, Route, useHistory, useLocation } from 'react-router-dom'
+import '../i18n';
+import { useTranslation } from 'react-i18next';
 import useNavigation from '../misc/navigation';
-import Routes from '../misc/routes';
+import C19Logo from '../assets/images/c-19_logo.png'
+import UserLogo from '../assets/images/user.png'
 
 const Header = (props: any) => {
     const location = useLocation();
     const navigation = useNavigation();
+    const { t } = useTranslation();
 
     const [title, setTitle] = React.useState('');
 
@@ -15,11 +19,19 @@ const Header = (props: any) => {
         setTitle(location.pathname);
     }, [location])
 
-    return (<>
-        <div id='qt-header'>
-            <h3>{title}</h3>
-        </div>
-    </>
+    return (
+        <>
+            <Row id='qt-header'>
+                <Image src={C19Logo} />
+                <span className='header-font my-auto mx-1'>{t('translation:title')}</span>
+                {/* <h3>{title}</h3> */}
+            </Row>
+            <Row id='user-container'>
+                <Image src={UserLogo} />
+                <span className='my-auto mx-1'>{'{$user-name}'}</span>
+
+            </Row>
+        </>
     )
 }
 
