ace76bc332c602126d8f928934b0c505f83ae46d
Artur T <81235055+a-trzewik@users.noreply.github.com>
2021-03-30 14:11:42 +0200

    #25 api proxy config (#34)

--- src/keycloak.tsx
+++ src/keycloak.tsx
@@ -23,6 +23,6 @@ import Keycloak from 'keycloak-js'
  
 // Initialize keycloak
 
-const keycloak = Keycloak('https://quicktest-6155b8d7d3c6.coronawarn.app/api/config/keycloak.json');
+const keycloak = Keycloak('/api/config/keycloak.json');
 
 export default keycloak
