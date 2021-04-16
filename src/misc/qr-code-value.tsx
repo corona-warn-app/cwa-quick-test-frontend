eb9448430aad810083cbfbc888da70554ed87e25
a-trzewik <a.trzewik@t-systems.com>
2021-04-16 12:04:40 +0200

    add salt and hash to qrcode

--- src/misc/qr-code-value.tsx
+++ src/misc/qr-code-value.tsx
@@ -1,13 +1,36 @@
+/*
+ * Corona-Warn-App / cwa-quick-test-frontend
+ *
+ * (C) 2021, T-Systems International GmbH
+ *
+ * Deutsche Telekom AG and all other contributors /
+ * copyright owners license this file to you under the Apache
+ * License, Version 2.0 (the "License"); you may not use this
+ * file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 import { timeStamp } from 'node:console';
-import React from 'react'
 import Patient from './patient';
+import CryptoJS from 'crypto-js';
 
 export interface IQRCodeValue {
     fn: string,
     ln: string,
     dob: string, //"1990-01-01",   - >day of birth
-    guid: string,
-    timestamp: number
+    testid: string,
+    timestamp: number,
+    salt: string, // 32 Bit random in HEX
+    hash?: string // SHA256 Hash
 }
 
 const baseUrl = 'https://s.coronawarn.app?v=1#';
@@ -20,9 +43,18 @@ export const getQrCodeValueString = (guid: string, fn: string = '', ln: string =
         fn: fn,
         ln: ln,
         dob: dob ? dob.toISOString().split('T')[0] : '',
-        guid: guid,
-        timestamp: Date.now()
+        testid: guid,
+        timestamp: Date.now(),
+        salt:  CryptoJS.lib.WordArray.random(128 / 8).toString(CryptoJS.enc.Hex)
     }
+
+    // const value: IQRCodeValue = { "fn": "Erika", "ln": "Mustermann", "dob": "1990-12-23", "timestamp": 1618386548, "testid": "52cddd8e-ff32-4478-af64-cb867cea1db5", "salt": "759F8FF3554F0E1BBF6EFF8DE298D9E9" }
+
+    const shaEntry = `${value.dob}#${value.fn}#${value.ln}#${value.timestamp.toString()}#${value.testid}#${value.salt}`;
+    value.hash = CryptoJS.SHA256(shaEntry).toString(CryptoJS.enc.Hex);
+
+    // console.log("The hash: "+value.hash);
+
 
     const json = JSON.stringify(value);
     encodedJson = btoa(json);
