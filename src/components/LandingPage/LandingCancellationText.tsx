6b4e528367a78b89bef102a67d74cd0749ebd3ea
Gordon Grund <gordon.grund@t-systems.com>
2022-10-21 12:37:21 +0200

    adjust text and display

--- src/components/LandingPage/LandingCancellationText.tsx
+++ src/components/LandingPage/LandingCancellationText.tsx
@@ -1,12 +1,28 @@
+import React from 'react';
+import { Accordion, AccordionCollapse, AccordionToggle, Form } from 'react-bootstrap';
 import useGetCancellationText, { CancellationTextType } from '../../misc/useGetCancellationText';
 
 const LandingCancellationText = (props: any) => {
   const cancellationText = useGetCancellationText(CancellationTextType.LANDING);
+  const [collapsed, setCollapsed] = React.useState(false);
 
   return (
     <>
       {cancellationText && (
-        <p dangerouslySetInnerHTML={{ __html: cancellationText }} className='cancellation-text mb-5' />
+        <Accordion className='mb-5 mx-auto' onSelect={(evt) => setCollapsed(evt !== null)}>
+          <AccordionToggle as={Form.Label} className='w-100' eventKey='0'>
+            <p
+              dangerouslySetInnerHTML={{
+                __html: `${cancellationText[0]} ${!collapsed ? '(...)' : ''}`,
+              }}
+              className='cancellation-text'
+            ></p>
+          </AccordionToggle>
+
+          <AccordionCollapse eventKey='0'>
+            <p dangerouslySetInnerHTML={{ __html: cancellationText[1] }} className='cancellation-text ' />
+          </AccordionCollapse>
+        </Accordion>
       )}
     </>
   );
