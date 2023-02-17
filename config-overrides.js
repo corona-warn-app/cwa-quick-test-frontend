34fcc340a312941b6223d6877168e8f6e09095ef
Gordon Grund <gordon.grund@t-systems.com>
2023-02-17 16:50:39 +0100

    add webpack plugin process+buffer

---
+++ config-overrides.js
@@ -0,0 +1,11 @@
+const webpack = require('webpack');
+
+module.exports = function override(config) {
+  config.plugins = (config.plugins || []).concat([
+    new webpack.ProvidePlugin({
+      process: 'process/browser',
+      Buffer: ['buffer', 'Buffer'],
+    }),
+  ]);
+  return config;
+};
