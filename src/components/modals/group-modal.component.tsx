93c7d18d3a622bad062e7bffe3315c260ef0f228
Gordon Grund <gordon.grund@t-systems.com>
2023-02-24 15:14:40 +0100

    add pattern to groupname

--- src/components/modals/group-modal.component.tsx
+++ src/components/modals/group-modal.component.tsx
@@ -365,6 +365,7 @@ const GroupModal = (props: any) => {
                   updateGroupProp('name', evt.target.value);
                   props.resetError();
                 }}
+                pattern={'^[\\w]*$'}
                 maxLength={45}
                 isInvalid={props.isCreationError}
                 InvalidText={t('translation:group-conflict-error')}
